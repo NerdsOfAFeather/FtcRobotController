eb2300e3fa7c575a0f37191483ba8c45b7fa0a28
gavin-kv <gavin.kapesvanslyke@gmail.com>
2026-09-11 16:08:47 -0400

    Fix motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeConfig.java
@@ -37,10 +37,10 @@ public abstract class DecodeConfig extends DecodeObjectDetection {
         rightFrontDrive = hardwareMap.get(DcMotorEx.class, "FrontRightDrive");
         rightBackDrive = hardwareMap.get(DcMotorEx.class, "BackRightDrive");
 
-        leftFrontDrive.setDirection(Direction.FORWARD);
+        leftFrontDrive.setDirection(Direction.REVERSE);
         leftBackDrive.setDirection(Direction.FORWARD);
         rightFrontDrive.setDirection(Direction.REVERSE);
-        rightBackDrive.setDirection(Direction.REVERSE);
+        rightBackDrive.setDirection(Direction.FORWARD);
 
         leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
